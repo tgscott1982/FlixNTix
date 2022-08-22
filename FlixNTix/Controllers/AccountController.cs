189c404b7d79b95d1697534cbea1942342648a41
Travis Gersch <t_gersch@hotmail.com>
2022-08-22 16:23:10 -0500

    created register view model created registration view pages created register and logout buttons

--- FlixNTix/Controllers/AccountController.cs
+++ FlixNTix/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using FlixNTix.Data;
+using FlixNTix.Data.Static;
 using FlixNTix.Data.ViewModels;
 using FlixNTix.Models;
 using Microsoft.AspNetCore.Identity;
@@ -46,5 +47,47 @@ public class AccountController : Controller
 
         TempData["Error"] = "The User Name Or Password Is Incorrect. Please Try Again!";
         return View(loginVM);
+    }
+
+    public IActionResult Register()
+    {
+        return View(new RegisterVM());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterVM registerVM)
+    {
+
+        if (!ModelState.IsValid) return View(registerVM);
+
+        var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+
+        if (user != null)
+        {
+            TempData["Error"] = "This Email Is Already Registered";
+            return View(registerVM);
+        }
+
+        var newUser = new ApplicationUser()
+        {
+            FullName = registerVM.FullName,
+            Email = registerVM.EmailAddress,
+            UserName = registerVM.EmailAddress
+        };
+        var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+        if (newUserResponse.Succeeded)
+            await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+        return View("RegistrationCompleted");
+        
+
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Movie");
     }
 }
---
+++ FlixNTix/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlixNTix.Data.ViewModels;
+
+public class RegisterVM
+{
+    [Display(Name = "Full Name")]
+    [Required(ErrorMessage = "Full Name Is Required")]
+    public string FullName { get; set; }
+
+    [Display(Name = "Email Address")]
+    [Required(ErrorMessage = "Email Address Is Required")]
+    public string EmailAddress { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Display(Name ="Confirm Password")]
+    [Required(ErrorMessage = "Confirm Password Field Is Required")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage ="Passwords Do Not Match")]
+    public string ConfirmPassword { get; set; }
+}
